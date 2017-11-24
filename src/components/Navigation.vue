2409362fcaaa0b8251a71619730e811136ef024e
Thijs Bouwes <thijsbouwes@gmail.com>
2017-11-24 09:38:59 +0000

    Updated the app title

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,7 +3,7 @@
 		<div class="navbar-fixed">
 			<nav>
 				<div class="nav-wrapper">
-					<a href="#!" class="brand-logo center"><i class="large material-icons">home</i> Insights</a>
+					<a href="#!" class="brand-logo center"><i class="large material-icons">home</i> Comfort</a>
 					<ul class="left">
 						<li><a href="#" data-activates="slide-out" class="button-collapse-sidebar"><i class="material-icons">menu</i></a></li>
 					</ul>
