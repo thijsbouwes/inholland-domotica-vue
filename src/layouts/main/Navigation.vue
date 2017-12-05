488c37a2455dea1a982be4a269aa8a6ea3e75597
Thijs Bouwes <thijsbouwes@gmail.com>
2017-12-05 21:59:30 +0100

    Fixed image path and updated the url in the meta data

--- src/layouts/main/Navigation.vue
+++ src/layouts/main/Navigation.vue
@@ -2,7 +2,7 @@
 	<div class="navbar-fixed">
 		<nav>
 			<div class="nav-wrapper">
-				<a href="#!" class="brand-logo center"><img src="/src/assets/home_comfort.png"> Comfort</a>
+				<a href="#!" class="brand-logo center"><img src="/static/home_comfort.png"> Comfort</a>
 				<ul class="left">
 					<li><a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a></li>
 				</ul>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -10,7 +10,7 @@
             </div>
 
             <div class="col s12 m6 l3">
-                <heater></heater>
+                <!--<heater></heater>-->
             </div>
 
             <div class="col s12 m6 l3">
@@ -29,11 +29,11 @@
     import Windows from '../components/Windows';
     import Heater from '../components/Heater';
     import ActionButton from '../components/ActionButton';
-    import RemoteHouse from '../components/RemoteHouse';
-    import Modal from '../components/Modal';
+    // import RemoteHouse from '../components/RemoteHouse';
+    // import Modal from '../components/Modal';
     import {ENDPOINTS} from '../config/api';
 
     export default {
-        components: { Lamps, Windows, ActionButton, Layout, Heater, RemoteHouse, Modal },
+        components: { Lamps, Windows, ActionButton, Layout, Heater },
     }
 </script>
