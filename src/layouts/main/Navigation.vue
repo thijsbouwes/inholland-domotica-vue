4710bd992295f04d8b8af9eb163be96f4761f1c4
Thijs Bouwes <thijsbouwes@gmail.com>
2017-12-09 21:01:58 +0100

    Added the fullscreen option to the navbar

--- src/layouts/main/Navigation.vue
+++ src/layouts/main/Navigation.vue
@@ -6,7 +6,64 @@
 				<ul class="left">
 					<li><a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a></li>
 				</ul>
+				<ul class="right">
+					<li><a href="#" @click="toggleFullScreen"><i class="material-icons">{{ icon }}</i></a></li>
+				</ul>
 			</div>
 		</nav>
 	</div>
 </template>
+
+
+<script>
+	export default {
+	    data() {
+	        return {
+	            icon: 'fullscreen',
+                full_screen: false
+			}
+		},
+
+		methods: {
+            toggleFullScreen() {
+                if (this.full_screen === false) {
+                    this.enterFullScreen();
+                    this.icon = 'fullscreen_exit';
+                    this.full_screen = true;
+				} else {
+                    this.exitFullscreen();
+                    this.icon = 'fullscreen';
+                    this.full_screen = false;
+				}
+			},
+
+			enterFullScreen() {
+                let elem = document.documentElement;
+
+                if (elem.requestFullscreen) {
+                    elem.requestFullscreen();
+                } else if (elem.msRequestFullscreen) {
+                    elem.msRequestFullscreen();
+                } else if (elem.mozRequestFullScreen) {
+                    elem.mozRequestFullScreen();
+                } else if (elem.webkitRequestFullscreen) {
+                    elem.webkitRequestFullscreen();
+                }
+			},
+
+			exitFullscreen() {
+                let elem = document;
+
+                if (elem.cancelFullScreen) {
+                    elem.cancelFullScreen();
+                } else if (elem.msExitFullscreen) {
+                    elem.msExitFullscreen();
+                } else if (elem.mozCancelFullScreen) {
+                    elem.mozCancelFullScreen();
+                } else if (elem.webkitCancelFullScreen) {
+                    elem.webkitCancelFullScreen();
+                }
+            }
+		}
+	}
+</script>
