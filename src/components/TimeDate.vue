ec96bca871aa5efaed1bc6b835354234ccd06ff1
Thijs Bouwes <thijsbouwes@gmail.com>
2017-12-09 20:30:04 +0100

    Added the time date module

---
+++ src/components/TimeDate.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="card">
+            <div class="card-content">
+                <div class="house-item center">
+                    <i class="material-icons small">access_time</i>
+                    <h5>{{ time }}</h5>
+                </div>
+                <div class="house-item center">
+                    <i class="material-icons small">date_range</i>
+                    <h6>{{ date }}</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {ENDPOINTS} from "../config/api";
+    import moment from 'moment';
+
+    export default {
+        data() {
+            return {
+                date: '',
+                time: '',
+                dateFormat: 'DD-MM-YYYY',
+                timeFormat: 'HH:mm:ss',
+                timeZone: 'nl',
+            }
+        },
+
+        created() {
+            this.refreshTime();
+            moment.locale(this.timeZone);
+            setInterval(this.refreshTime, 1000);
+        },
+
+        methods: {
+            refreshTime() {
+                this.date = moment().format(this.dateFormat);
+                this.time = moment().format(this.timeFormat);
+            },
+        }
+    }
+</script>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -13,9 +13,27 @@
                 <heater></heater>
             </div>
 
+            <div class="col s6 m6 l3 x3">
+                <time-date></time-date>
+            </div>
+        </div>
+
+        <div class="row">
             <div class="col s12 m6 l3">
                 <a class="waves-effect waves-light btn modal-trigger" href="#remote">Modal</a>
                 <modal target_id="remote"><remote-house></remote-house></modal>
+            </div>
+
+            <div class="col s12 m6 l3">
+
+            </div>
+
+            <div class="col s12 m6 l3">
+
+            </div>
+
+            <div class="col s12 m6 l3">
+
             </div>
         </div>
 
@@ -31,10 +49,11 @@
     import Heater from '../components/Heater';
     import ActionButton from '../components/ActionButton';
     import RemoteHouse from '../components/RemoteHouse';
+    import TimeDate from '../components/TimeDate';
     import Modal from '../components/Modal';
     import {ENDPOINTS} from '../config/api';
 
     export default {
-        components: { Lamps, Windows, ActionButton, Layout, Heater, Modal, RemoteHouse },
+        components: { Lamps, Windows, ActionButton, Layout, Heater, Modal, RemoteHouse, TimeDate },
     }
 </script>
